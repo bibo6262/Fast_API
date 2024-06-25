b82993da19c1cd1f2feba053ffea1b5ecc22b82f
bibo6262 <bollywoodmotion6262@gmail.com>
2024-06-25 22:45:04 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI , HTTPException
 from pydantic import BaseModel
 from typing import List
+from uuid import uuid4 , UUID
 
 app=FastAPI()
 
 class job(BaseModel):
+    id:UUID
     company_name :str
     job_tittle:str
     job_Description:str
@@ -19,30 +21,52 @@ class job(BaseModel):
 jobs=[]
 
 @app.post("/jobs/")
-
 def create_job(job:job):
+    job.id=uuid4()
     jobs.append(job)
     return job
     
-    
-
-@app.get("/jobs/")
-
+@app.get("/")
 def get_one():
     return jobs
 
 
 @app.get("/jobs/{job_id}")
-
-def read_job(job_id:int):
-    if job_id >=len(jobs) or job_id<0:
-        raise HTTPException(status_code=404,detail="job not found")
+def read_job(job_id: UUID):
     
-    return jobs[job_id]
+    for job in jobs:
+        if job.id == job_id:
+            
+           return job
+    raise HTTPException(status_code=404,detail="job not found")
+    
+
+@app.put("/jobs/{job_id}")
+
+def update_job(job_id:UUID,update_job:job):
+    
+    for i, job in enumerate(jobs):
+        
+        if job.id == job_id:
+            jobs[i] =update_job
+            jobs[i].id = job_id
+            
+            return update_job
+    raise HTTPException(status_code=404,detail="job not found")    
+
+
+@app.delete("/jobs/{job_id}")
+
+def delete_job(job_id:UUID):
+    for i , job in enumerate(jobs):
+        if job.id==job_id:
+            delete_job=jobs.pop(i)
+            return delete_job
+        
+    raise HTTPException(status_code=404,detail="job not found")    
+
+
    
-
-
-
 if __name__ =="__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0" ,port=8000)
